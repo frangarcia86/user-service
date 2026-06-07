c83bf7a409f1c1c8a67335ff72a194ae4fc06025
Fran Garcia <frangarcia86@gmail.com>
2026-06-07 16:41:29 +0200

    [CONTROLLER] Added user resource

--- src/main/java/com/users/api/mapper/UserDtoMapper.java
+++ src/main/java/com/users/api/mapper/UserDtoMapper.java
@@ -1,5 +1,25 @@
 package com.users.api.mapper;
 
-public class UserDtoMapper {
-    
+import java.time.Instant;
+import java.util.UUID;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+import com.users.api.dto.CreateUserRequest;
+import com.users.api.dto.UserResponse;
+import com.users.domain.model.User;
+
+@Mapper(
+		componentModel = MappingConstants.ComponentModel.CDI,
+		imports = {UUID.class, Instant.class}
+)
+public interface UserDtoMapper {
+
+	@Mapping(target = "id", expression = "java(UUID.randomUUID())")
+	@Mapping(target = "createdAt", expression = "java(Instant.now())")
+	User toDomain(CreateUserRequest request);
+
+	UserResponse toResponse(User user);
 }
